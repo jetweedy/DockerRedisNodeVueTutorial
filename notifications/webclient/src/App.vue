baaf83c4661c646f1123fe5bb7a3ef20c7013ab8
Jon Tweedy <jetweedy@gmail.com>
2021-05-11 14:19:31 -0400

    M

---
+++ notifications/webclient/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<hr/>
+<h3>
+  {{message}}
+</h3>
+<hr/>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: "",
+    }
+  },
+  created(){
+    try {
+      const ws = new WebSocket("ws://localhost:3000/");
+      ws.onmessage = ({data}) => {
+        this.message =  data;
+        console.log(this.message);
+      }
+    } catch(err) {
+      console.log(err);
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
